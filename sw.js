c948f433c4a2f17fe74dac32406b0d86339feb70
Yakir-hazan <noyahazan2022@gmail.com>
2026-07-21 13:43:11 +0300

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'madhom-v2';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/db.js',
+  './js/app.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Cache-first for app shell, network-first fallback for everything else
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request)
+        .then((res) => {
+          const resClone = res.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, resClone));
+          return res;
+        })
+        .catch(() => cached);
+    })
+  );
+});
