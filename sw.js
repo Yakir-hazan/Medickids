e1135e6996acbd238c5192448e096d2c19fa55a1
Yakir-hazan <noyahazan2022@gmail.com>
2026-07-26 13:51:08 +0300

    chore: bump cache to madhom-v22

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
    (increment the -vNN suffix here whenever APP_VERSION changes there, e.g. 'v17' here when
    APP_VERSION becomes '1.0.0-beta.2'). Without this bump, users' devices keep serving old
    cached files and "בדוק אם יש עדכון" in Settings will report "already up to date" even when it isn't. */
-const CACHE_NAME = 'madhom-v21';
+const CACHE_NAME = 'madhom-v22';
 const APP_SHELL = [
   './',
   './index.html',
